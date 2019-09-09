ba87f42ab6920ebd385021082af707953c39b571
janKozie1 <jankkoziel@gmail.com>
2019-09-09 13:01:12 +0200

    Dropdown select and input

--- src/components/Routes/Admin/Form/index.js
+++ src/components/Routes/Admin/Form/index.js
@@ -1,17 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import * as S from './styledComponents'
+import { userInfo } from 'os'
+
+const OPTIONS = [
+    {
+        text: 'id',
+        _id: 'id'
+    },
+    {
+        text: 'imię',
+        _id: 'name'
+    },
+    {
+        text: 'grupa',
+        _id: 'group'
+    },
+    {
+        text: 'scieżka',
+        _id: 'path'
+    }
+]
 
 const Form = () => {
+    let [expanded, setExpanded] = useState(false)
+    let [current, setCurrent] = useState(OPTIONS[0])
+    let [userInput, setUserInput] = useState('')
+    let changeOption = _id => {
+        setCurrent(OPTIONS.find(e => e._id === _id))
+        setExpanded(false)
+    }
+    let fetchData = async => {
+        console.log(current._id, userInput)
+    }
     return (
         <S.Form>
-            <S.Select>
-                <S.Option>Ścieżka</S.Option>
-                <S.Option>ID</S.Option>
-                <S.Option>Imie</S.Option>
-                <S.Option>Grupa</S.Option>
+            <S.Select expanded={expanded}>
+                <S.Option onClick={() => setExpanded(expanded => !expanded)}>
+                    {current.text}
+                    <S.ExpandIcon />
+                </S.Option>
+                {OPTIONS.map(e => {
+                    return (
+                        <S.Option
+                            key={e._id}
+                            onClick={() => changeOption(e._id)}>
+                            {e.text}
+                        </S.Option>
+                    )
+                })}
             </S.Select>
-            <S.Input></S.Input>
+            <S.InputContainer>
+                <S.Input
+                    placeholder={'Szukana fraza'}
+                    value={userInput}
+                    onChange={({ target: { value } }) =>
+                        setUserInput(value)
+                    }></S.Input>
+                <S.FindIcon onClick={() => fetchData()} />
+            </S.InputContainer>
         </S.Form>
     )
 }
--- src/components/Routes/Admin/Form/styledComponents.js
+++ src/components/Routes/Admin/Form/styledComponents.js
@@ -1,4 +1,6 @@
 import styled, { css } from 'styled-components'
+import { ReactComponent as expand } from '../../../../assets/arrow.svg'
+import { ReactComponent as find } from '../../../../assets/find.svg'
 
 export const Form = styled.form`
     margin-top: ${({ theme: { sizes } }) => css`
@@ -11,8 +13,17 @@ export const Select = styled.div`
     display: grid;
     grid-auto-rows: 30px;
     height: 30px;
-    width: 70px;
+    width: 110px;
     overflow: hidden;
+    margin-right: 10px;
+    ${({ expanded }) =>
+        expanded &&
+        css`
+            height: max-content;
+            svg {
+                transform: rotate(180deg);
+            }
+        `}
 `
 
 export const Option = styled.div`
@@ -20,22 +31,51 @@ export const Option = styled.div`
     height: 100%;
     display: flex;
     align-items: center;
-    justify-content: center;
-    border: 2px solid black;
-    border-right: 0;
+    justify-content: flex-start;
+    padding: 0 10px;
+    cursor: pointer;
+    color: #666;
+    text-transform: lowercase;
+    border: 1px solid #ddd;
+    border-top-width: 0px;
+    border-bottom-width: 0;
+    &:last-child {
+        border-bottom-width: 1px;
+    }
+    &:hover {
+        background: rgba(0, 0, 0, 0.1);
+    }
+    &:first-child {
+        &:hover {
+            background: white;
+        }
+        border: 2px solid black;
+    }
+`
+export const ExpandIcon = styled(expand)`
+    width: 20px;
+    height: 20px;
+    margin-left: auto;
+    margin-right: 0;
+    transition: transform 0.3s;
 `
 
-export const Label = styled.label`
+export let FindIcon = styled(find)`
+    height: 80%;
+    padding: 0 10px;
+`
+
+export let InputContainer = styled.div`
+    height: 30px;
+    border: 2px solid black;
     display: flex;
-    flex-direction: column;
     align-items: center;
 `
-
-export const LabelTitle = styled.p``
-
 export const Input = styled.input`
-    height: 30px;
-    padding: 5px 10px;
-    border: 2px solid black;
+    height: 100%;
+    padding: 5px 0 5px 10px;
+    outline: none;
+    flex: 1;
+    border: 0;
     font-family: inherit;
 `
